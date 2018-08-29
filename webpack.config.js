bc39c3ec8e15f83bb04ce277da8c959c85e9e095
Shabiq Hazarika <hazarika.shabiq@gmail.com>
2018-08-29 23:13:52 +0530

    Added index.html file to the repository

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/entry.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist/js')
     },
     mode: 'production'
 }
